312ceb6bc6d199d909a44967063ba80a12716234
ba-91 <ba91@kakao.com>
2022-03-06 20:59:52 +0900

    add Navigate to a New Screen

--- lib/main.dart
+++ lib/main.dart
@@ -29,12 +29,48 @@ class _RandomWordsState extends State<RandomWords> {
   final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-      ),
+      appBar: AppBar(title: const Text('Startup Name Generator'), actions: [
+        IconButton(
+          icon: Icon(Icons.list),
+          onPressed: _pushSaved,
+          tooltip: 'Saved Suggestions',
+        )
+      ]),
       body: ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
@@ -59,7 +95,7 @@ class _RandomWordsState extends State<RandomWords> {
             ),
             onTap: () {
               setState(() {
-                if(alreadySaved) {
+                if (alreadySaved) {
                   _saved.remove(_suggestions[index]);
                 } else {
                   _saved.add(_suggestions[index]);
